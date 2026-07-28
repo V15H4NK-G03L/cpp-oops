aebb110662d1afb53671d0ee6d1e2043acd05abd
Vishank Goel (GitK) <vishankgoel11@gmail.com>
2026-07-28 18:58:40 +0530

    added getter and setter functions

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -14,21 +14,34 @@ class Student {
 
     private:
     string collegeName;
-    void bunk(){
+    void bunk() const{
         cout << this->name << " is bunking class" << endl;
     }
+    string noChange;
 
     public:
     Student(){
         cout << "Default Constructor" << endl;
     }
 
-    Student(int age, string name, int id, string collegeName){
+    Student(int age, string name, int id, string collegeName,string noChange){
         cout << "Parameterised ctor" << endl;
         this->id = id;
         this->age=age;
         this->name=name;
         this->collegeName=collegeName;
+        this->noChange=noChange;
+    }
+
+    void setCollege(string collegeName){
+        this->collegeName=collegeName;
+        cout << "College New Name Set" << endl;
+    }
+
+    void getHidden() const{
+        cout << this->collegeName << endl;
+        this->bunk();
+        cout << this->noChange << endl;
     }
 
     ~Student(){
@@ -38,17 +51,19 @@ class Student {
 
 
 int main() {
-    cout << "Hi" << endl;
-    Student stA;
-    stA.age = 24;
-    // stA.collegeName = "ABC";
-    // stA.bunk();
-    cout << stA.age << endl;
-    cout << stA.name << endl;
-    cout << stA.id << endl;
+    // cout << "Hi" << endl;
+    // Student stA;
+    // stA.age = 24;
+    // // stA.collegeName = "ABC";
+    // // stA.bunk();
+    // cout << stA.age << endl;
+    // cout << stA.name << endl;
+    // cout << stA.id << endl;
 
-    Student B(24,"NoName",12345,"NoCollege");
+    Student B(24,"NoName",12345,"NoCollege","HelloNoChange");
     //B.test();
+    B.setCollege("NewTech College");
+    B.getHidden();
 
     return 0;
 }
