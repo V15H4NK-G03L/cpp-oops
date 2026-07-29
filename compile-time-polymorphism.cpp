5b28663c1437daa2de0248ec3410ad3d253a4f19
Vishank Goel (GitK) <vishankgoel11@gmail.com>
2026-07-29 18:31:21 +0530

    operator overloading implemented

--- compile-time-polymorphism.cpp
+++ compile-time-polymorphism.cpp
@@ -23,12 +23,46 @@ class Add {
     }
 };
 
+class Complex {
+    public:
+    int real;
+    int imag;
+
+    Complex(){
+        real = imag = -1;
+    }
+
+    Complex(int r, int i){
+        real = r;
+        imag = i;
+    }
+
+    void print(){
+        printf("%d + i%d\n", this->real, this->imag);
+    }
+
+    Complex operator+(const Complex &B){
+        Complex temp;
+        temp.real = this->real + B.real;
+        temp.imag = this->imag + B.imag;
+        return temp;
+    }
+};
+
 int main() {
-    int x=5,y=6,z=10;
-    double a=2.12,b=3.45;
-    Add add;
-    cout << add.sum(x,y) << endl;
-    cout << add.sum(x,y,z) << endl;
-    cout << add.sum(a,b) << endl;
+    // int x=5,y=6,z=10;
+    // double a=2.12,b=3.45;
+    // Add add;
+    // cout << add.sum(x,y) << endl;
+    // cout << add.sum(x,y,z) << endl;
+    // cout << add.sum(a,b) << endl;
+
+    Complex A(2,5);
+    A.print();
+    Complex B(3,2);
+    B.print();
+    Complex C = A + B;
+    C.print();
+
     return 0;
 }
